4cc8d57d42450d9811a13fb270588206861e79cc
necros <necros83@gmail.com>
2024-01-30 17:49:24 +0700

    fix review

--- conftest.py
+++ conftest.py
@@ -22,5 +22,5 @@ def check_resolution_browser(request):
     browser.config.window_height = resolution_height
     if resolution == (1920, 1080) or resolution == (3200, 1800) or resolution == (1680, 1050):
         return True
-    elif resolution == (768, 1280) or resolution == (540, 960) or resolution == (390, 844):
+    elif resolution == (640, 960) or resolution == (540, 960) or resolution == (390, 844):
         return False
--- tests/test_skip.py
+++ tests/test_skip.py
@@ -8,18 +8,20 @@ from selene import browser
 
 @pytest.mark.desktop
 def test_github_desktop(check_resolution_browser):
-    if check_resolution_browser:
+    if not check_resolution_browser:
+        pytest.skip('Mobile')
+    else:
         browser.open('https://github.com')
         browser.element('.HeaderMenu-link--sign-in').click()
-    else:
-        pytest.skip('Mobile')
+
 
 
 @pytest.mark.mobile
 def test_github_mobile(check_resolution_browser):
-    if not check_resolution_browser:
+    if check_resolution_browser:
+        pytest.skip('Desktop')
+    else:
         browser.open('https://github.com')
         browser.element('.Button--link').click()
         browser.element('.HeaderMenu-link--sign-in').click()
-    else:
-        pytest.skip('Desktop')
+
